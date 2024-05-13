03bc26bff5cfcfa5a1ae56c7209477d4f3ced95f
Breymar34 <79845808+Breymar34@users.noreply.github.com>
2024-05-13 09:56:02 +0800

    *

---
+++ base.php
@@ -0,0 +1,31 @@
+<?php
+class Crud{
+    private $server = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db = "threewmadtwo";
+    private $conn; // MySQLi connection object
+
+    public function __construct()
+    {
+        // Open db connection
+        $this->conn = new mysqli($this->server, $this->username, $this->password, $this->db);
+
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+        echo "Connected successfully";
+    }
+
+    public function __destruct()
+    {
+        // Close db connection
+        if ($this->conn) {
+            $this->conn->close();
+            echo "Connection closed";
+        }
+    }
+}
+
+$crud = new Crud();
